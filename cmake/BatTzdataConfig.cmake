40514811f2794ff9b10a36316278019dff343533
Dausel, Martin <Martin.Dausel@barco.com>
2021-05-04 17:51:09 +0200

    PRJ0406-3824-Fix-CMake-Config-file-Package-support-in-BTB
    - Created Cmake project files with install step config package file support. see
    
    https://cmake.org/cmake/help/latest/guide/importing-exporting/index.html#exporting-targets
    https://cmake.org/cmake/help/latest/guide/using-dependencies/index.html#libraries-providing-config-file-packages
    https://cmake.org/cmake/help/latest/manual/cmake-packages.7.html
    https://cmake.org/cmake/help/latest/command/find_package.html

---
+++ cmake/BatTzdataConfig.cmake
@@ -0,0 +1,7 @@
+#
+# Copyright Barco N.V. All Rights Reserved.
+#
+
+# tzdata has no dependencies to other projects so there is nothin to do here
+include("${CMAKE_CURRENT_LIST_DIR}/BatTzdataTargets.cmake")
+
